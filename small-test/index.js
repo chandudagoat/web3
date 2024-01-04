09a3d5e2c72836a47463181db4c0cb3655cf39bf
hclanka <152237163+chandudagoat@users.noreply.github.com>
2024-01-04 23:17:20 +0530

    learned about public and private keys

--- small-test/index.js
+++ small-test/index.js
@@ -5,5 +5,10 @@ const {
   connectionApiUrl,
   Transaction,
   Keypair,
-  LAMPORTS_PER_SOL
-} = require("@solana/web3.js")
+  LAMPORTS_PER_SOL,
+} = require("@solana/web3.js");
+
+const dev_keys = new Keypair();
+const pub_key = new PublicKey(dev_keys._keypair.publicKey).toString();
+const private_key = dev_keys._keypair.secretKey;
+console.log(pub_key, private_key);
