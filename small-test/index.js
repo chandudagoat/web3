137f6d3d0b7c2b6f225d0bf2ec24b9af4ad1ebd6
hclanka <152237163+chandudagoat@users.noreply.github.com>
2024-01-04 23:24:38 +0530

    getting wallet balance

--- small-test/index.js
+++ small-test/index.js
@@ -6,9 +6,25 @@ const {
   Transaction,
   Keypair,
   LAMPORTS_PER_SOL,
+  clusterApiUrl,
 } = require("@solana/web3.js");
 
 const dev_keys = new Keypair();
 const pub_key = new PublicKey(dev_keys._keypair.publicKey).toString();
 const private_key = dev_keys._keypair.secretKey;
-console.log(pub_key, private_key);
+
+const getWalletBalance = async () => {
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+  const my_wallet = Keypair.fromSecretKey(private_key);
+  const balance = await connection.getBalance(
+    new PublicKey(my_wallet.publicKey)
+  );
+
+  console.log(
+    `Wallet address is ${my_wallet.publicKey} and my balance is ${balance}`
+  );
+
+  return balance;
+};
+
+getWalletBalance();
